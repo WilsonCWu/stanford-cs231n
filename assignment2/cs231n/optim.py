6b78e8626b8b9f5ff99267e6d601d1261b595208
Wilson Wu <wilsoncwu@Wilsons-MacBook-Pro.local>
2024-07-05 18:29:04 -0400

    fcn

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -69,7 +69,8 @@ def sgd_momentum(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    v = config["momentum"] * v - config["learning_rate"] * dw
+    next_w = w + v
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -107,7 +108,9 @@ def rmsprop(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    cache = config["decay_rate"] * config["cache"] + (1 - config["decay_rate"]) * dw**2
+    next_w = w - config["learning_rate"] * dw / (np.sqrt(cache) + config["epsilon"])
+    config["cache"] = cache
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -152,7 +155,15 @@ def adam(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    t = config["t"] + 1
+    m = config["beta1"] * config["m"] + (1 - config["beta1"]) * dw
+    v = config["beta2"] * config["v"] + (1 - config["beta2"]) * dw**2
+    m_hat = m / (1 - config["beta1"]**t)
+    v_hat = v / (1 - config["beta2"]**t)
+    next_w = w - config["learning_rate"] * m_hat / (np.sqrt(v_hat) + config["epsilon"])
+    config["t"] = t
+    config["m"] = m
+    config["v"] = v
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
